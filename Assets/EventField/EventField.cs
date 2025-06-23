7f57f2538712388996b4d55aead653790f7f53de
jafarchik <jafarovja@yandex.ru>
2025-06-24 00:51:31 +0700

    add event field

---
+++ Assets/EventField/EventField.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace Virsign.EventField
+{
+    public class EventField<T>
+    {
+        private T _value;
+
+        public event Action<T> OnValueChanged;
+
+        public EventField(T initialValue = default)
+        {
+            _value = initialValue;
+        }
+
+        public void SetValue(T newValue)
+        {
+            if (Equals(_value, newValue))
+            {
+                return;
+            }
+
+            _value = newValue;
+            OnValueChanged?.Invoke(_value);
+        }
+
+        public T GetValue()
+        {
+            return _value;
+        }
+    }
+}
