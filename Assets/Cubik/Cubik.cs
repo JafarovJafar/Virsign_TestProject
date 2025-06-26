cfd81367c08269b429d9c65a3c67313245f8bc99
jafarchik <jafarovja@yandex.ru>
2025-06-26 23:56:08 +0700

    doliv

--- Assets/Cubik/Cubik.cs
+++ Assets/Cubik/Cubik.cs
@@ -1,19 +1,8 @@
-using Shafir.FSM;
 using UnityEngine;
 
 namespace Virsign
 {
     public class Cubik : MonoBehaviour
     {
-    }
-
-    public class CubikContext
-    {
-
-    }
-
-    public class CubikAppearState : IState
-    {
-
     }
 }
