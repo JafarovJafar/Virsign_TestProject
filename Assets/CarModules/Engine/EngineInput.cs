9eb8defac72ef0b4598858e20cfcd63f01c8c249
Jafarov Jafar <48131258+JafarovJafar@users.noreply.github.com>
2026-02-26 21:17:14 +0700

    Revert "Sprint 1 (#1)"
    This reverts commit c640bbd579463cf51a9af8788b927bf6802a6adc.

--- Assets/CarModules/Engine/EngineInput.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Virsign
-{
-    public class EngineInput
-    {
-        public EventField<bool> Start = new();
-        public EventField<bool> TurnOff = new();
-        public EventField<float> GasRatio = new();
-    }
-}
